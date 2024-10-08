137fdc9f5e39a1a0d15b4f643d80857ee5d91a51
TeeVeeoAI <tosi07001@utb.vaxjo.se>
2024-10-08 10:56:34 +0200

    added Factorial

--- Program.cs
+++ Program.cs
@@ -1,30 +1,54 @@
-﻿using System.Runtime.InteropServices;
-
-Main();
+﻿while(true) {
+    Main();
+    if (Console.ReadLine() == "!") {
+        break;
+    } else {
+        
+    }
+}
 
 void Main() {
-    double[] input = new double[2];
-    Console.Write("Enter your first value: ");
-    input[0] = Convert.ToDouble(Console.ReadLine());
-    Console.Write("Enter your second value: ");
-    input[1] = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("What do you want to do with them?");
-    char type = Convert.ToChar(Console.ReadLine());
-    while (true) {
-        if (type == '+') {
-            Console.WriteLine(input[0] + " + " + input[1] + " = " + Addition(input[0], input[1]));
-            break;
-        } else if (type == '-') {
-            Console.WriteLine(input[0] + " - " + input[1] + " = " + Subtraction(input[0], input[1])); 
-            break;
-        } else if (type == '*') {
-            Console.WriteLine(input[0] + " * " + input[1] + " = " + Multiplication(input[0], input[1]));
-            break;
-        } else if (type == '/') {
-            Console.WriteLine(input[0] + " / " + input[1] + " = " + Divided(input[0], input[1]));
-            break;
-        } else {
-            Console.WriteLine("input is invalid");
+    Console.WriteLine("How meny numbers do you need?");
+    int amount = Convert.ToInt32(Console.ReadLine());
+    if (amount == 2) {
+        double[] input = new double[2];
+        Console.Write("Enter your first value: ");
+        input[0] = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter your second value: ");
+        input[1] = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("What do you want to do with them?");
+        while (true) {
+            string type = Convert.ToString(Console.ReadLine());
+            if (type == "+") {
+                Console.WriteLine(input[0] + " + " + input[1] + " = " + Addition(input[0], input[1]));
+                break;
+            } else if (type == "-") {
+                Console.WriteLine(input[0] + " - " + input[1] + " = " + Subtraction(input[0], input[1])); 
+                break;
+            } else if (type == "*") {
+                Console.WriteLine(input[0] + " * " + input[1] + " = " + Multiplication(input[0], input[1]));
+                break;
+            } else if (type == "/") {
+                Console.WriteLine(input[0] + " / " + input[1] + " = " + Divided(input[0], input[1]));
+                break;
+            } else {
+                Console.WriteLine("input is invalid");
+            }
+        }
+        return;
+    } else if (amount == 1) {
+        Console.Write("Enter your value: ");
+        double input = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("What do you want to do with it?");
+        while (true) {
+            string type = Convert.ToString(Console.ReadLine());
+            if (type == "!") {
+                int input_int = Convert.ToInt32(input);
+                Console.WriteLine(input + "! = " + Factorial(input_int));
+                break;
+            } else {
+                Console.WriteLine("input is invalid");
+            }
         }
     }
 }
@@ -43,4 +67,11 @@ double Multiplication(double a, double b) {
 
 double Divided(double a, double b) {
     return a/b;
+}
+
+int Factorial(int a) {
+    for (int i = a-1; i >= 1; i--) {
+        a = a*i;
+    }
+    return a;
 }
